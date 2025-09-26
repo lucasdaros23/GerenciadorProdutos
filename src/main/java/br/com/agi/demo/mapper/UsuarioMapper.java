b293858ec5b318cfb124c0ebbe541b789a08ba74
lucas <lucasdarosparente@gmail.com>
2025-09-26 11:59:27 -0300

    pROJETO

---
+++ src/main/java/br/com/agi/demo/mapper/UsuarioMapper.java
@@ -0,0 +1,9 @@
+package br.com.agi.demo.mapper;
+
+import br.com.agi.demo.dto.requests.CriarUsuarioRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioMapper {
+    public static Usuario map(final CriarUsuarioRequest request)
+}
--- src/main/java/br/com/agi/demo/service/UsuarioService.java
+++ src/main/java/br/com/agi/demo/service/UsuarioService.java
@@ -2,11 +2,21 @@ package br.com.agi.demo.service;
 
 
 import br.com.agi.demo.dto.response.BaseResponse;
+import br.com.agi.demo.repository.UsuarioRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 public class UsuarioService {
+
+
+    private final UsuarioMapper usuarioMapper;
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioMapper usuarioMapper, UsuarioRepository usuarioRepository) {
+        this.usuarioMapper = usuarioMapper;
+        this.usuarioRepository = usuarioRepository;
+    }
 
     public BaseResponse criarUsuario
 
